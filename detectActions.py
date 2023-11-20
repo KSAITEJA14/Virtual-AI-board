c8a1d50b8a848227b92b28cca5c00eaf253f6150
VSCMourya <mouryavsvp@gmail.com>
2023-11-20 17:27:59 -0600

    optimised code

--- detectActions.py
+++ detectActions.py
@@ -1,110 +1,51 @@
-
-import os 
+import os
 from math import sqrt
-    
 
-def detectAction(hand_landmarks,image_shape):
+def detectAction(hand_landmarks, image_shape):
     """
-    Input: 
-    hand_landmarks: information about the hand keypoints
-    image_shape: shape of the image
-
-    Description: 
-    This function checks and concludes the state of the user (Erasing, drawing, pause)
-    """ 
-    
-    if erase(hand_landmarks,image_shape):
+    Detects the user's action (Erasing, Drawing, or Unknown) based on hand landmarks and image shape.
+    """
+    if isErasing(hand_landmarks, image_shape):
         return "Erase"
-    
-    if isDrawing(hand_landmarks,image_shape):
+    if isDrawing(hand_landmarks, image_shape):
         return "Draw"
-    
     return "unknown"
 
-def calc_distance(p1, p2):
-    
+def calcDistance(p1, p2):
     """
-    Input: 
-    p1 : [x1,y1]
-    p2 = [x2,y2]
-
-    Description: 
-    This function calculates the distance betwen two points
-    
+    Calculates the Euclidean distance between two points.
     """
-    
-    # simple function, I hope you are more comfortable
-    return sqrt((p1[0]-p2[0])**2+(p1[1]-p2[1])**2)
+    return sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
-def erase(hand_landmarks,image_shape):
+def isErasing(hand_landmarks, image_shape):
     """
-    Input: 
-    hand_landmarks: information about the hand keypoints
-    image_shape: shape of the image
-
-    Description: 
-    This function checks the conditions for erasing  
+    Checks if the erasing conditions are met.
     """
-    h,w = image_shape
-    index_finger_leftmost_x = (hand_landmarks.landmark[8].x)*w
-    index_finger_leftmost_y = (hand_landmarks.landmark[8].y)*h
-
-    middle_finger_tip_x = (hand_landmarks.landmark[12].x)*w
-    middle_finger_tip_y = (hand_landmarks.landmark[12].y)*h
-
-    index_finger_MCP_x = (hand_landmarks.landmark[5].x)*w
-    index_finger_MCP_y = (hand_landmarks.landmark[5].y)*h
-
-    pointA = (index_finger_leftmost_x, index_finger_leftmost_y) 
-    pointB = (middle_finger_tip_x, middle_finger_tip_y) 
-    pointC = (index_finger_MCP_x, index_finger_MCP_y) 
+    h, w = image_shape
+    index_tip = (hand_landmarks.landmark[8].x * w, hand_landmarks.landmark[8].y * h)
+    middle_tip = (hand_landmarks.landmark[12].x * w, hand_landmarks.landmark[12].y * h)
+    index_mcp = (hand_landmarks.landmark[5].x * w, hand_landmarks.landmark[5].y * h)
     
-    distance1 = calc_distance(pointA, pointB) 
-    distance2 = calc_distance(pointA, pointC) 
-    return distance1 < distance2//3
+    return calcDistance(index_tip, middle_tip) < calcDistance(index_tip, index_mcp) // 3
 
-def isDrawing(hand_landmarks,image_shape):
+def isDrawing(hand_landmarks, image_shape):
     """
-    Input: 
-    hand_landmarks: information about the hand keypoints
-    image_shape: shape of the image
-
-    Description: 
-    This function checks the conditions for drawing 
+    Checks if the drawing conditions are met.
     """
+    w = image_shape[1]
+    thumb_tip_x = hand_landmarks.landmark[4].x * w
+    index_mcp_x = hand_landmarks.landmark[2].x * w
 
-    _,w = image_shape
+    return thumb_tip_x > index_mcp_x
 
-    thumb_tip_x = (hand_landmarks.landmark[4].x)*w
-    index_finger_MCP_x = (hand_landmarks.landmark[2].x)*w
+def checkColor(hand_landmarks, image_shape, color_change, color_num):
+    """
+    Checks for a change in pen color.
+    """
+    h, w = image_shape
+    index_tip_y = hand_landmarks.landmark[8].y * h
+    index_mcp_y = hand_landmarks.landmark[5].y * h
+    wrist_y = hand_landmarks.landmark[0].y * h
 
-    return thumb_tip_x > index_finger_MCP_x
-
-def checkColor(hand_landmarks,image_shape,color_change,color_num):
-    
-    '''
-    Input: 
-    hand_landmarks: information about the hand keypoints
-    image_shape: shape of the image
-    color_change: A default value (False) for color change
-    color_num: a number denoting a specific color
-    
-    Description: 
-    This function checks whether the user has changed the pen color. If they wish to, itll change
-    the pen color.
-    '''
-
-    h,w = image_shape 
-    curr_result = False
-
-    index_finger_leftmost_y = (hand_landmarks.landmark[8].y)*h
-    index_finger_MCP_y = (hand_landmarks.landmark[5].y)*h
-    wrist_y = (hand_landmarks.landmark[0].y)*h
-
-    if wrist_y > index_finger_leftmost_y > index_finger_MCP_y:
-        curr_result = True
-
-    if color_change == curr_result:
-        return color_num, False
-
-    return color_num+1,True
+    curr_result = wrist_y > index_tip_y > index_mcp_y
+    return (color_num + 1, True) if curr_result != color_change else (color_num, False)
--- draw_vid.py
+++ draw_vid.py
@@ -4,81 +4,37 @@ import mediapipe as mp
 import numpy as np
 from detectActions import detectAction, checkColor
 
-# def mediapipe_results(frame_infos):
-def mediapipe_results(frame,circles,color_change,color_num,pen_color,pen_size,mpHands,hands,mp_draw):
+def mediapipe_results(frame, circles, color_change, color_num, pen_color, pen_size, mpHands, hands, mp_draw):
 
     flip = True 
     eraser_size = 100
-   
-    ## Stores previously drawing circles to give continous lines
-    pen_color_changes = {0:"(255,0,0)",1:"(0,255,0)",2:"(0,0,255)",3:"(0,0,0)"}
+    pen_color_changes = {0: "(255,0,0)", 1: "(0,255,0)", 2: "(0,0,255)", 3: "(0,0,0)"}
 
-    if flip : 
-        frame = cv2.flip(frame,1)
-        
-    h,w,c = frame.shape
+    frame = cv2.flip(frame, 1) if flip else frame
+    h, w, c = frame.shape
+    frame = cv2.resize(frame, (w*2, h*2))
 
-    h = h*2
-    w = w*2
-    frame = cv2.resize(frame,(w,h))
-
-    # frame = np.zeros([h,w,c],dtype=np.uint8)
-    # frame.fill(255)
-
-    img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    #processes the keypoints of the hand
-    results = hands.process(img_rgb)
-
-    if not results.multi_hand_landmarks : 
-        pass
-    else : 
-        for hand_landmarks in results.multi_hand_landmarks :
+    results = hands.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mpHands.HAND_CONNECTIONS)
+            action = detectAction(hand_landmarks, (h*2, w*2))
+            color_num, color_change = checkColor(hand_landmarks, (h*2, w*2), color_change, color_num)
+            pen_color = eval(pen_color_changes[color_num % 4]) if color_change else pen_color
 
-            # detects the type of action done 
-            action = detectAction(hand_landmarks,(h,w))
-
-            color_num, color_change = checkColor(hand_landmarks,(h,w),color_change,color_num)
-            
-            if color_change:
-                pen_color = eval(pen_color_changes[color_num%4])
-            
-            ## Draw mode
-            if action == 'Draw': 
-                index_x = hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x
-                index_y = hand_landmarks.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y
-                pos = [(int(index_x*w), int(index_y*h)),eval(pen_color_changes[color_num%4])]
-               
-                cv2.circle(frame, pos[0], 20, (0,0,0), 2)
-                circles.append(pos)
-
-            ## Erase mode
+            if action == 'Draw':
+                index_pos = (int(hand_landmarks.landmark[8].x * w*2), int(hand_landmarks.landmark[8].y * h*2))
+                cv2.circle(frame, index_pos, 20, (0,0,0), 2)
+                circles.append([index_pos, eval(pen_color_changes[color_num % 4])])
             elif action == 'Erase':
-                eraser_mid = [
-                        int(hand_landmarks.landmark[8].x * w),
-                        int(hand_landmarks.landmark[8].y * h)
-                    ]
-               
-                bottom_right = (eraser_mid[0]+eraser_size, eraser_mid[1]+eraser_size)
-                top_left = (eraser_mid[0]-eraser_size, eraser_mid[1]-eraser_size)
-
+                eraser_mid = (int(hand_landmarks.landmark[8].x * w*2), int(hand_landmarks.landmark[8].y * h*2))
+                top_left = (eraser_mid[0] - eraser_size, eraser_mid[1] - eraser_size)
+                bottom_right = (eraser_mid[0] + eraser_size, eraser_mid[1] + eraser_size)
                 cv2.rectangle(frame, top_left, bottom_right, (0,0,255), 5)
-                
-                try : 
-                    for pt in range(len(circles)):
-                        if circles[pt][0][0]>top_left[0] and circles[pt][0][0]<bottom_right[0]: 
-                            if circles[pt][0][1]>top_left[1] and circles[pt][0][1]<bottom_right[1]:
-                                circles.pop(pt)
-                except IndexError : 
-                    pass
-            
-            elif action == "pause": 
-                
-                index_x = hand_landmarks.landmark[8].x
-                index_y = hand_landmarks.landmark[8].y
-                pos = (int(index_x*w), int(index_y*h))
-
+                circles = [c for c in circles if not (top_left[0] < c[0][0] < bottom_right[0] and top_left[1] < c[0][1] < bottom_right[1])]
+            elif action == "pause":
+                pos = (int(hand_landmarks.landmark[8].x * w*2), int(hand_landmarks.landmark[8].y * h*2))
                 cv2.circle(frame, pos, 20, (0,0,255), 2)
 
     return frame, circles, pen_size, pen_color, color_change, color_num
